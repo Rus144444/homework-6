adf3b6c677c0a19d879a37b1ca0428e61aa99e2a
Ruslan <az144444z@mail.com>
2024-11-16 09:53:07 +0100

    lesson #2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,44 @@
-// import { galleryItems } from "./gallery-items.js";
-// Change code below this line
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
 
-const galleryEl = document.querySelector(".gallery");
-console.log("Number of categories:", galleryEl.children.length); // #1
+const list = document.querySelector(".ingredients");
 
-const itemsGallery = [...galleryEl.children];
-itemsGallery.forEach((e) => {
-  console.log(`
-    Category: ${e.firstElementChild.textContent} 
-    Elements: ${e.lastElementChild.children.length}
-  `);
+// #1
+
+// const markup = ingredients
+//   .map((item) => `<li class='item'>${item}</li>`)
+//   .join("");
+// console.log(markup);
+// list.insertAdjacentHTML("beforebegin", markup);
+
+// #2
+
+// const markup = [];
+
+// for (let i = 0; i < ingredients.length; i++) {
+//   const item = document.createElement("li");
+//   item.classList.add("item");
+//   item.textContent = ingredients[i];
+//   markup.push(item);
+// }
+
+// console.log(markup);
+// list.append(...markup);
+
+// #3
+
+const markup = ingredients.map((el) => {
+  const item = document.createElement("li");
+  item.classList.add("item");
+  item.textContent = el;
+  return item;
 });
+
+console.log(markup);
+list.append(...markup);
